f1e4a695ac79028ebe00cad532f8df1813ccbf0f
Pradipa Y <pdpyyo@gmail.com>
2025-11-07 15:32:25 +0700

    mengganti design di beranda

--- app/src/main/java/com/example/questnavigastugas_103/ui/theme/Color.kt
+++ app/src/main/java/com/example/questnavigastugas_103/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val RedPrimary = Color(0xFFDF0139)
+val BlackDeep = Color(0xFF1E1E27)
+val BlackSoft = Color(0xFF28242A)
+val GrayLight = Color(0xFFE2E2E2)
--- app/src/main/java/com/example/questnavigastugas_103/view/Beranda.kt
+++ app/src/main/java/com/example/questnavigastugas_103/view/Beranda.kt
@@ -1,19 +1,10 @@
 package com.example.questnavigastugas_103.view
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -25,13 +16,19 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.questnavigastugas_103.R
+import androidx.compose.ui.graphics.Color
+import com.example.questnavigastugas_103.ui.theme.BlackDeep
+import com.example.questnavigastugas_103.ui.theme.RedPrimary
 
 @Composable
 fun Beranda(onSubmitClick: () -> Unit) {
-    Scaffold { inner ->
+    Scaffold(
+        containerColor = BlackDeep
+    ) { inner ->
         Column(
             modifier = Modifier
                 .fillMaxSize()
+                .background(BlackDeep)
                 .padding(inner)
                 .padding(24.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
@@ -59,6 +56,10 @@ fun Beranda(onSubmitClick: () -> Unit) {
             Button(
                 onClick = onSubmitClick,
                 shape = MaterialTheme.shapes.extraLarge,
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = RedPrimary,
+                    contentColor = Color.White
+                ),
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(52.dp)
