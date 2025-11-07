37b88397da7b051fdabdf773abff8b7186e6844a
Pradipa Y <pdpyyo@gmail.com>
2025-11-07 13:47:37 +0700

    menambahkan Button di function ListData di file ListData.kt

--- app/src/main/java/com/example/questnavigastugas_103/view/ListData.kt
+++ app/src/main/java/com/example/questnavigastugas_103/view/ListData.kt
@@ -3,10 +3,13 @@ package com.example.questnavigastugas_103.view
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -65,7 +68,13 @@ fun ListData(
                     }
                 }
             }
-
+            Button(
+                onClick = toBeranda,
+                shape = MaterialTheme.shapes.extraLarge,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(48.dp)
+            ) { Text(stringResource(R.string.beranda)) }
         }
     }
 }
