1687ef7d89c92bfac79cdff6d63d684990082a68
Pradipa Y <pdpyyo@gmail.com>
2025-11-07 13:18:53 +0700

    menambahkan variabel di function FormPendaftaran di file Form.kt

--- app/src/main/java/com/example/questnavigastugas_103/view/Form.kt
+++ app/src/main/java/com/example/questnavigastugas_103/view/Form.kt
@@ -1,10 +1,23 @@
 package com.example.questnavigastugas_103.view
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 
 @Composable
 fun FormPendaftaran(
-
+    onSukses: (Peserta) -> Unit,
+    onBatal: () -> Unit,
+    onBack: () -> Unit
 ){
+    var nama by rememberSaveable { mutableStateOf("") }
+    var alamat by rememberSaveable { mutableStateOf("") }
+    var jk by rememberSaveable { mutableStateOf("") }
+    var status by rememberSaveable { mutableStateOf("") }
+    var showDialog by remember { mutableStateOf(false) }
 
+    val valid = nama.isNotBlank() && jk.isNotBlank() && status.isNotBlank() && alamat.isNotBlank()
 }
