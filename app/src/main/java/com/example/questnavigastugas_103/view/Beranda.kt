25bc7f19e28762b411c637593f7eea02a30ff615
Pradipa Y <pdpyyo@gmail.com>
2025-11-07 13:11:31 +0700

    menambahkan library untuk isi dari column di function beranda di file Beranda.kt

--- app/src/main/java/com/example/questnavigastugas_103/view/Beranda.kt
+++ app/src/main/java/com/example/questnavigastugas_103/view/Beranda.kt
@@ -16,7 +16,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.questnavigastugas_103.R
 
 @Composable
