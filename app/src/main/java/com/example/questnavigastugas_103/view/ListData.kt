f14b33e91af40bd8ac346c91f0fa1dee19e9808f
Pradipa Y <pdpyyo@gmail.com>
2025-11-07 13:42:50 +0700

    menambahkan scaffold di function ListData di file ListData.kt

--- app/src/main/java/com/example/questnavigastugas_103/view/ListData.kt
+++ app/src/main/java/com/example/questnavigastugas_103/view/ListData.kt
@@ -4,7 +4,17 @@ import androidx.compose.runtime.Composable
 
 @Composable
 fun ListData(
-
+    items: List<Peserta>,
+    toBeranda: () -> Unit,
+    toForm: () -> Unit
 ){
-
+    Scaffold(
+        topBar = { SmallTopAppBar(title = { Text(stringResource(R.string.list_title)) }) }
+    ) { inner ->
+        Column(
+            modifier = Modifier
+                .padding(inner)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        )
 }
